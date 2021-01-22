6fb232fdc88d7dfbf5454a25dcdcc7c3f49eeb06
Vitalygorkov <vet55omsk@gmail.com>
2021-01-23 00:12:35 +0500

    added task description and linking tasks with other tasks

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 class Task(models.Model):
     task_text = models.CharField(max_length=200)
+    task_descr = models.CharField(max_length=900, null= True, blank=True)
     pub_date = models.DateTimeField('date published')
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+                                        # on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.task_text
@@ -11,6 +15,8 @@ class Task(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+class Place(models.Model):
+    place = models.CharField(max_length=200)
 
 class TaskUnder(models.Model):
     taskunder = models.ForeignKey(Task, on_delete=models.CASCADE)
