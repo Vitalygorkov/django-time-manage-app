40ebe0de132971338442157692fb79b767e03b6d
Vitalygorkov <vet55omsk@gmail.com>
2021-01-23 20:48:14 +0500

    made assignment of tasks to places as a many-to-many relationship

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 
+class Place(models.Model):
+    place_name = models.CharField(max_length=200)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    # task_in_place = models.ManyToManyField(Task, blank=True)
+    # task_in_place = models.ManyToManyField(Task, null=True, blank=True)
+    def __str__(self):
+        return self.place_name
 
 class Task(models.Model):
     task_text = models.CharField(max_length=200)
-    task_descr = models.CharField(max_length=900, null= True, blank=True)
+    task_descr = models.CharField(max_length=900, default='Нет описания')
+    target_date = models.DateField('target date', null= True, blank=True)
     pub_date = models.DateTimeField('date published')
-    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    task_completed = models.BooleanField(default=False)
+    parent_task = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, blank=True)
+    places = models.ManyToManyField(Place)
 
-                                        # on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__(self):
         return self.task_text
@@ -15,8 +26,20 @@ class Task(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-class Place(models.Model):
-    place = models.CharField(max_length=200)
+class Category(models.Model):
+    category_name = models.CharField(max_length=200)
+    cat_descr = models.CharField(max_length=900)
+    # target_date = models.DateField('target date', null= True, blank=True)
+    # pub_date = models.DateTimeField('date published')
+    # task_completed = models.BooleanField(default=False)
+    # parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+
+
+
+    def __str__(self):
+        return self.category_name
+
 
 class TaskUnder(models.Model):
     taskunder = models.ForeignKey(Task, on_delete=models.CASCADE)
--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -1,15 +1,19 @@
 from django.http import HttpResponse, Http404
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from .models import Task,TaskUnder
+from .models import Task,Category, Place
 
 
 
 def index(request):
-    latest_task_list = Task.objects.order_by('-pub_date')[:5]
+    latest_task_list = Task.objects.order_by('-pub_date')
+    category_list = Category.objects.all()
+    place_list = Place.objects.all()
     template = loader.get_template('tasks/index.html')
     context = {
         'latest_task_list': latest_task_list,
+        'category_list': category_list,
+        'place_list': place_list,
     }
     return HttpResponse(template.render(context, request))
 
