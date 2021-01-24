adce257272a8189d6b1d9ceb42ac298ec3398ed2
Vitalygorkov <vet55omsk@gmail.com>
2021-01-24 10:23:14 +0500

    improving the appearance of the displayed task lists.

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Place(models.Model):
     place_name = models.CharField(max_length=200)
     parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    tasks = models.ManyToManyField('Task')
     # task_in_place = models.ManyToManyField(Task, blank=True)
     # task_in_place = models.ManyToManyField(Task, null=True, blank=True)
     def __str__(self):
